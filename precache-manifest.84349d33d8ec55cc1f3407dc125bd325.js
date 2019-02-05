4aaa3b01ca9ae66e4c79aed8601e7f700a641180
Malamud-A <malamud.a98@gmail.com>
2019-02-05 13:56:07 +0200

    Updates

---
+++ precache-manifest.84349d33d8ec55cc1f3407dc125bd325.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0c46bb250bfc314977c",
+    "url": "/authors-catalog/static/css/main.8e183bbd.chunk.css"
+  },
+  {
+    "revision": "c0c46bb250bfc314977c",
+    "url": "/authors-catalog/static/js/main.c0c46bb2.chunk.js"
+  },
+  {
+    "revision": "65336bbdacd75b80637b",
+    "url": "/authors-catalog/static/js/1.65336bbd.chunk.js"
+  },
+  {
+    "revision": "5900dc32e54882d86c58",
+    "url": "/authors-catalog/static/js/runtime~main.5900dc32.js"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/authors-catalog/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/authors-catalog/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/authors-catalog/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "b505e201f8e3d467db033222a0382b3c",
+    "url": "/authors-catalog/index.html"
+  }
+];
